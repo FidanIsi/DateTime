8bc0ef5fe2a21b2bcbc1b46f80817d509542b4a9
fidan <fidanji@code.edu.az>
2023-08-25 16:39:02 +0400

    adding hour

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@
             string input = Console.ReadLine();
             var date = DateTime.ParseExact(input, "dd/MM/yyyy", null);
             DateTime newDate = date.AddDays(30).AddHours(6).AddMinutes(17);
-            Console.WriteLine("New date: " + newDate.ToString("dd/MM/yyyy"));
+            Console.WriteLine("New date: " + newDate);
         }
     }
 }
