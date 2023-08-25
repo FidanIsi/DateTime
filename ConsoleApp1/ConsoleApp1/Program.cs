ad186f69f97b0d4b5be190c5acb6501d11274690
fidan <fidanji@code.edu.az>
2023-08-25 16:14:40 +0400

    new dateTime added

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a date in dd/MM/yyyy format: ");
+            string input = Console.ReadLine();
+            var date = DateTime.ParseExact(input, "dd/MM/yyyy", null);
+            DateTime newDate = date.AddDays(30).AddHours(6).AddMinutes(17);
+            Console.WriteLine("New date: " + newDate.ToString("dd/MM/yyyy"));
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a date in dd | MM | yyyy format");
+            string input = Console.ReadLine();
+
+            try
+            {
+                var date = DateTime.ParseExact(input, "dd | MM | yyyy", null);
+                Console.WriteLine("Date: " + date.ToString("dd | MM | yyyy"));
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            } 
+        }
+    }
+}
